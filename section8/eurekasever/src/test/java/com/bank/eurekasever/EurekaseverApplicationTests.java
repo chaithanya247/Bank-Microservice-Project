20f692426795ae53cda26d5e269e9e02b6bd71f2
chaithanya247 <chaithanyareddy247@gmail.com>
2026-07-25 21:01:34 -0400

    adding section8 code

---
+++ section8/eurekasever/src/test/java/com/bank/eurekasever/EurekaseverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.eurekasever;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaseverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
